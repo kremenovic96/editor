a57c42f397bb809f68cc76097a989f07174bda6c
kremenovic96 <ranko.kremenovic@gmail.com>
2018-05-06 21:09:01 +0200

    can type multiple chars starting at top left(need more work on this)

--- editor/Editor.java
+++ editor/Editor.java
@@ -1,13 +1,138 @@
 package editor;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Editor extends Application {
+    private static final int WINDOW_WIDTH = 500;
+    private static final int WINDOW_HEIGHT = 500;
+    private List<Text> listOfText = new ArrayList<>();
+
+    private class KeyEventHandler implements EventHandler<KeyEvent> {
+        int textX;
+        int textY;
+
+        private static final int STARTING_FONT_SIZE = 20;
+        private int STARTING_TEXT_POSITION_X = 250;
+        private int STARTING_TEXT_POSITION_Y = 250;
+
+        /**
+         * The Text to display on the screen.
+         */
+        private Text displayText = new Text(STARTING_TEXT_POSITION_X, STARTING_TEXT_POSITION_Y, "");
+        private int fontSize = STARTING_FONT_SIZE;
+        Group rootCopy;
+        private String fontName = "Verdana";
+        KeyEventHandler(final Group root, int windowWidth, int windowHeight) {
+            textX = 5;
+            textY = 5;
+
+            // Initialize some empty text and add it to root so that it will be displayed.
+            displayText = new Text(textX, textY, "");
+            listOfText.add(displayText);
+            // Always set the text origin to be VPos.TOP! Setting the origin to be VPos.TOP means
+            // that when the text is assigned a y-position, that position corresponds to the
+            // highest position across all letters (for example, the top of a letter like "I", as
+            // opposed to the top of a letter like "e"), which makes calculating positions much
+            // simpler!
+            displayText.setTextOrigin(VPos.TOP);
+            displayText.setFont(Font.font(fontName, fontSize));
+
+            // All new Nodes need to be added to the root in order to be displayed.
+            rootCopy = root;
+            //root.getChildren().addAll(listOfText);
+            rootCopy.getChildren().addAll(listOfText);
+        }
+
+        @Override
+        public void handle(KeyEvent keyEvent) {
+            if (keyEvent.getEventType() == KeyEvent.KEY_TYPED) {
+                // Use the KEY_TYPED event rather than KEY_PRESSED for letter keys, because with
+                // the KEY_TYPED event, javafx handles the "Shift" key and associated
+                // capitalization.
+                String characterTyped = keyEvent.getCharacter();
+                if (characterTyped.length() > 0 && characterTyped.charAt(0) != 8) {
+                    // Ignore control keys, which have non-zero length, as well as the backspace
+                    // key, which is represented as a character of value = 8 on Windows.
+                    //displayText.setText(characterTyped);
+                    //STARTING_TEXT_POSITION_X++;
+                    displayText = new Text(textX, textY, characterTyped);
+                    //listOfText.add(displayText);
+                    System.out.println(listOfText.size());
+                   // System.out.println(listOfText);
+                    keyEvent.consume();
+                }
+
+                centerText();
+            } else if (keyEvent.getEventType() == KeyEvent.KEY_PRESSED) {
+                // Arrow keys should be processed using the KEY_PRESSED event, because KEY_PRESSED
+                // events have a code that we can check (KEY_TYPED events don't have an associated
+                // KeyCode).
+                KeyCode code = keyEvent.getCode();
+                if (code == KeyCode.UP) {
+                    fontSize += 5;
+                    for(Text t : listOfText)
+                        t.setFont(Font.font(fontName, fontSize));
+                    //displayText.setFont(Font.font(fontName, fontSize));
+                    //centerText();
+                } else if (code == KeyCode.DOWN) {
+                    fontSize = Math.max(0, fontSize - 5);
+                    for(Text t : listOfText)
+                        t.setFont(Font.font(fontName, fontSize));
+                    //displayText.setFont(Font.font(fontName, fontSize));
+                    //centerText();
+                }
+                else if (code == KeyCode.BACK_SPACE){
+                    rootCopy.getChildren().remove(rootCopy.getChildren().size()-1);
+                    textX -=
+                }
+            }
+        }
+
+        private void centerText() {
+            // Figure out the size of the current text.
+            double textHeight = displayText.getLayoutBounds().getHeight();
+            double textWidth = displayText.getLayoutBounds().getWidth();
+
+            // Calculate the position so that the text will be centered on the screen.
+            double textTop =textY+10;
+            double textLeft = textX += textWidth; // textCenterX - textWidth / 2;
+
+            // Re-position the text.
+            displayText.setX(textLeft);
+            displayText.setY(textTop);
+            listOfText.add(displayText);
+            // Make sure the text appears in front of any other objects you might add.
+            displayText.toFront();
+            rootCopy.getChildren().add(displayText);
+
+
+        }
+    }
     @Override
     public void start(Stage primaryStage) {
-
+        Group root = new Group();
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT, Color.WHITE);
+        EventHandler<KeyEvent> keyEventHandler =
+                new KeyEventHandler(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+        scene.setOnKeyTyped(keyEventHandler);
+        scene.setOnKeyPressed(keyEventHandler);
+        primaryStage.setTitle("Editor");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
